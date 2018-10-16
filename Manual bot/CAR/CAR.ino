1fa70f2852e601db03a2e91e9be5a550838ae755
aayush-ag21 <43410137+aayush-ag21@users.noreply.github.com>
2018-10-17 02:22:50 +0530

    Update CAR.ino

--- Manual bot/CAR/CAR.ino
+++ Manual bot/CAR/CAR.ino
@@ -1,24 +1,22 @@
 #include <XBOXUSB.h>
-
 #ifdef dobogusinclude
 #include <spi4teensy3.h>
 #endif
 #include <SPI.h>
-                                                                byte TF=8;int TS=9;byte TB=10;
 
 byte LF=2;int LS=3; byte LB=4;byte RF=5;int RS=6;byte RB=7;
 int x=0,y=0;
 int rs=0,ls=0;
-                                                                
+
 USB Usb;
 XBOXUSB Xbox(&Usb);
 
 void setup() {
   Serial.begin(115200);
-#if !defined(__MIPSEL__)
-  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
-#endif
-  if (Usb.Init() == -1) {
+  #if !defined(__MIPSEL__)
+   while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+  #endif
+   if (Usb.Init() == -1) {
     Serial.print(F("\r\nOSC did not start"));
     while (1); //halt
   }
@@ -29,20 +27,21 @@ void setup() {
   pinMode(RF,OUTPUT);
   pinMode(RS,OUTPUT);
   pinMode(RB,OUTPUT);
-                                                                   pinMode(TF,OUTPUT);pinMode(TS,OUTPUT);pinMode(TB,OUTPUT);
   }
   void loop() {
    Usb.Task();
    if (Xbox.Xbox360Connected) {
     if (Xbox.getAnalogHat(RightHatX) != 0  || Xbox.getAnalogHat(RightHatY) != 0) {
       if (Xbox.getAnalogHat(RightHatX) != 0)
-        x=map(Xbox.getAnalogHat(RightHatX),-32512,32512,-200,200);
-        else
-        x=0;
+        x=map(Xbox.getAnalogHat(RightHatX),-32512,32512,-50,50);
+        else x=0;
       if (Xbox.getAnalogHat(RightHatY) != 0)
         y=map(Xbox.getAnalogHat(RightHatY),-32512,32512,-200,200);
+      else y=0;
+      }
       else
-      y=0;
+      {
+        x=0;y=0;
       }
       rs=y-x;
       ls=y+x;
@@ -50,41 +49,28 @@ void setup() {
       {
         digitalWrite(RF,HIGH);
         digitalWrite(RB,LOW);
-       }
+        analogWrite(RS,rs);
+      }
        
       if(rs<0)
       {
         digitalWrite(RF,LOW);
         digitalWrite(RB,HIGH);
-       }
+        analogWrite(RS,(-rs));
+      }
        
       if(ls>=0)
       {
         digitalWrite(LF,HIGH);
         digitalWrite(LB,LOW);
-       }
+        analogWrite(LS,ls);}
        
-      if(ls<0)
+      if(rs<0)
       {
         digitalWrite(LF,LOW);
         digitalWrite(LB,HIGH);
-      }
-      analogWrite(RS,rs);
-      analogWrite(LS,ls);      
-
-       if(y>=0)
-      {
-        digitalWrite(TF,HIGH);
-        digitalWrite(TB,LOW);
-       }
-       
-      if(y<0)
-      {
-        digitalWrite(TF,LOW);
-        digitalWrite(TB,HIGH);
-      }
-                                                 analogWrite(TS,y);
-                                                 Serial.println(y);
+        analogWrite(LS,(-ls));
+      }   
     }
-  delay(1);
+  delay(5);
 }
